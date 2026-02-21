79ee1a889bd51084534041658014e0fce14fcb2a
Hud2929 <hudwkim@gmail.com>
2026-02-21 11:53:26 -0500

    Initial Commit

---
+++ Pausely/PauselyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauselyApp.swift
+//  Pausely
+//
+//  Created by Hudson on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct PauselyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PauselyTests/PauselyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PauselyTests.swift
+//  PauselyTests
+//
+//  Created by Hudson on 2026-02-21.
+//
+
+import Testing
+@testable import Pausely
+
+struct PauselyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
